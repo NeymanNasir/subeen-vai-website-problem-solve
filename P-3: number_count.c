384678736c8da0b30ac812e73ede906ac977a7ec
Neyoman Nasir <nnnasir30@gmail.com>
2017-01-29 22:14:54 +0600

    Update P-3: number_count.c

--- P-3: number_count.c
+++ P-3: number_count.c
@@ -33,3 +33,30 @@ int count_numbers(char str[])
     }
     return (count+1);
 }
+
+/*another code by strol() function
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char line[1000000];
+    char *p,*e;
+    long input;
+    int count,N,T;
+    p=line;
+    scanf("%d",&T);
+    while(T--){
+            scanf(" %[^\n]",line);
+            count=0;
+        for(p=line; ;p=e){
+            input=strtol(p,&e,10);
+            if(p==e){
+            break;
+        }
+        count++;
+    }
+    printf("%d\n",count);
+    }
+    return 0;
+}
+*/
