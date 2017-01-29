e7e6b8bedcfaf70fc70bc0f2d0219efd4ed53dcd
Neyoman Nasir <nnnasir30@gmail.com>
2017-01-29 19:59:00 +0600

    Update P_31_vajok.c

--- P_31_vajok.c
+++ P_31_vajok.c
@@ -15,3 +15,31 @@ int main()
     }
     return 0;
 }
+
+//another code
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,j,num,root;
+    scanf("%d",&n);
+    for(j=1;j<=n;j++){
+        scanf("%d",&num);
+        printf("Case %d: ",j);
+        root=sqrt(num);
+        for(i=1;i<root;i++){
+            if(num%i==0){
+                printf("%d ",i);
+            }
+        }
+        for(i=root;i<=num;i++){
+            if(num%i==0){
+                printf("%d ",i);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+ekti number er jotogula vajok ba gunoniok thake sadharonoto tar ordhek square root er ceye choto
+ar baki ordhek tar square root er ceye boro.*/
