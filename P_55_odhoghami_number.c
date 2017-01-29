c521acd672b97525f0f9ede729fcd1b7afd14ff9
Neyoman Nasir <nnnasir30@gmail.com>
2017-01-29 19:36:11 +0600

    Update P_55_odhoghami_number.c

--- P_55_odhoghami_number.c
+++ P_55_odhoghami_number.c
@@ -11,3 +11,22 @@ int main()
     }
     return 0;
 }
+
+/**********************
+another code..
+    
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("1000\t");
+    for(i=999;i>0;i--){
+        if(i%5==0){
+            printf("\n");
+            }
+        printf("%d\t",i);
+    }
+    return 0;
+}
+
+5 er sob gunitok 5 ghor por por e ase*/
