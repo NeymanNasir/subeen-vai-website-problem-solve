e6d5db1ce1edef7af9c8bca8fb1ae5455ea312d2
Neyoman Nasir <nnnasir30@gmail.com>
2017-01-31 20:53:15 +0600

    Update P-7:squre_number.c

--- P-7:squre_number.c
+++ P-7:squre_number.c
@@ -16,3 +16,26 @@ int main()
     }
     return 0;
 }
+
+//by ceil() and floor() function
+
+/*#include<stdio.h>
+int main()
+{
+    int T,N;
+    double sq_root,difference;
+    scanf("%d",&T);
+    while(T--){
+        scanf("%d",&N);
+        sq_root=sqrt(N);
+        difference=ceil(sq_root)-floor(sq_root);
+        if(difference<0.000001){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
+*/
