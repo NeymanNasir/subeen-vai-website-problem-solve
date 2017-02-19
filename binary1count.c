60a2478d403722d29bfba334c03e91cd406dfd3a
Neyoman Nasir <nnnasir30@gmail.com>
2017-02-19 22:02:32 +0600

    Update binary1count.c

--- binary1count.c
+++ binary1count.c
@@ -7,13 +7,13 @@ int main()
     scanf("%d",&n);
     i=0;
     while(n>0){
-        str[i]=n%2+'0';
+        str[i]=n%2+'0';//assign integer to character in a string
         n=n/2;
         i++;
     }
     len=strlen(str);
     for(j=len-1,count=0;j>=0;j--){
-        printf("%c",str[j]);
+        printf("%c",str[j]);//print decimal to binary num
         if(str[j]=='1'){
             count++;
         }
